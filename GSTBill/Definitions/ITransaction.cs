9caa90b7cccc6875560953a60966735624199e9a
Tutu George <tutugeorge@gmail.com>
2017-07-14 01:16:43 +0530

    Added missing files

---
+++ GSTBill/Definitions/ITransaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSTBill.Definitions
+{
+    interface ITransaction
+    {
+        void Complete();
+        void Update();
+        void Cancel();
+
+        void AddItem();
+        void AddTender();
+        void CalculateTotal();
+    }
+}
