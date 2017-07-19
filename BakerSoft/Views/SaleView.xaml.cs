a933f4454e813dbc54e4a38d3c27139227d60752
snehgin <snehgin@gmail.com>
2017-07-19 06:45:19 +0530

    Auto Suggestion Text Box

--- BakerSoft/Views/SaleView.xaml.cs
+++ BakerSoft/Views/SaleView.xaml.cs
@@ -20,9 +20,66 @@ namespace GSTBill.Views
     /// </summary>
     public partial class SaleView : UserControl
     {
+        List<string> nameList;
         public SaleView()
         {
             InitializeComponent();
+            nameList = new List<string>
+            {
+                "AAAA",
+                "BBBB",
+                "CCCC"
+            };
+            txtAuto.TextChanged += new TextChangedEventHandler(txtAuto_TextChanged);
+        }
+
+        private void txtAuto_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string typedText = txtAuto.Text;
+            List<string> autoList = new List<string>();
+            autoList.Clear();
+
+            foreach (string item in nameList)
+            {
+                if (!string.IsNullOrEmpty(txtAuto.Text))
+                {
+                    if (item.StartsWith(typedText))
+                    {
+                        autoList.Add(item);
+                    }
+                }
+
+            }
+
+            if (autoList.Count > 0)
+            {
+                lbSuggestion.ItemsSource = autoList;
+                lbSuggestion.Visibility = Visibility.Visible;
+            }
+            else if (txtAuto.Text.Equals(""))
+            {
+                lbSuggestion.Visibility = Visibility.Collapsed;
+                lbSuggestion.ItemsSource = null;
+            }
+            else
+            {
+                lbSuggestion.Visibility = Visibility.Collapsed;
+                lbSuggestion.ItemsSource = null;
+            }
+        }
+
+        private void lbSuggestion_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbSuggestion.ItemsSource != null)
+            {
+                lbSuggestion.Visibility = Visibility.Collapsed;
+                txtAuto.TextChanged -= new TextChangedEventHandler(txtAuto_TextChanged);
+                if (lbSuggestion.SelectedIndex != -1)
+                {
+                    txtAuto.Text = lbSuggestion.SelectedItem.ToString();
+                }
+                txtAuto.TextChanged += new TextChangedEventHandler(txtAuto_TextChanged);
+            }
         }
     }
 }
