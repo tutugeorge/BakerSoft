0e6cbdc1042e3705860aaaa13c01e1d109fb6e39
Tutu George <tutugeorge@gmail.com>
2017-07-26 23:19:49 +0530

    Added Supplier Repository

---
+++ BakerSoft/Definitions/ISupplierRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakerSoft.Definitions
+{
+    interface ISupplierRepository
+    {
+        void AddSupplier();
+        void GetSuppliers();
+        void EditSupplier();
+    }
+}
---
+++ BakerSoft/Models/SupplierModel.cs
@@ -0,0 +1,28 @@
+﻿using BakerSoft.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakerSoft.Models
+{
+    class SupplierModel
+    {
+        private ISupplierRepository _supplierRepository;
+        public SupplierModel(ISupplierRepository supplierRepository)
+        {
+            _supplierRepository = supplierRepository;
+        }
+
+        public void AddSupplier()
+        {
+            _supplierRepository.AddSupplier();
+        }
+
+        public void GetSuppliers()
+        {
+            _supplierRepository.GetSuppliers();
+        }
+    }
+}
