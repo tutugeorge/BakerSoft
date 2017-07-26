301d84acf71c39ec5bfde808d5352f21680e6165
Tutu George <tutugeorge@gmail.com>
2017-07-26 23:23:26 +0530

    Updated Supplier

--- BakerSoft/Models/Supplier.cs
+++ BakerSoft/Models/Supplier.cs
@@ -8,6 +8,9 @@ namespace BakerSoft.Models
 {
     class Supplier
     {
-
+        public string Id { get; set; }
+        public string GSTIN { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 }
