af2e68f215274077de68e7d31e9f219804aa5bc4
Tutu George <tutugeorge@gmail.com>
2017-07-27 20:59:49 +0530

    Updated Add Purchase

--- BakerSoft/ViewModels/AddPurchaseViewModel.cs
+++ BakerSoft/ViewModels/AddPurchaseViewModel.cs
@@ -15,6 +15,36 @@ namespace BakerSoft.ViewModels
 
         public DelegateCommand PurchaseCmd { get; set; }
 
+        private string _purchasePrice;
+        public string PurchasePrice
+        {
+            get { return _purchasePrice; }
+            set { SetProperty(ref _purchasePrice, value); }
+        }
+        private string _quantity;
+        public string Quantity
+        {
+            get { return _quantity; }
+            set { SetProperty(ref _quantity, value); }
+        }
+        private string _supplierName;
+        public string SupplierName
+        {
+            get { return _supplierName; }
+            set { SetProperty(ref _supplierName, value); }
+        }
+        private string _productName;
+        public string ProductName
+        {
+            get { return _productName; }
+            set { SetProperty(ref _productName, value); }
+        }
+        private string _productId;
+        public string ProductId
+        {
+            get { return _productId; }
+            set { SetProperty(ref _productId, value); }
+        }
         public AddPurchaseViewModel(PurchaseTransaction purchaseTransaction)
         {
             _purchaseTransaction = purchaseTransaction;
