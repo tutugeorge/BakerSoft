07059fcd842739da93ec0a4d86627d57360b9135
Tutu George <tutugeorge@gmail.com>
2017-08-02 20:48:07 +0530

    Updated Add Product

--- BakerSoft/ViewModels/AddProductViewModel.cs
+++ BakerSoft/ViewModels/AddProductViewModel.cs
@@ -20,6 +20,49 @@ namespace BakerSoft.ViewModels
         public string SellingPrice { get; set;}
         public Tax ProductTax { get; set; }
         public List<Price> PriceList { get; set; }
+        private List<Tax> _taxRateList;
+        public List<Tax> TaxRateList
+        {
+            get
+            {
+                if(_taxRateList == null)
+                {
+                    _taxRateList = new List<Tax>()
+                    {
+                        new Tax() { CGST = 0.05, SGST = 0.05 },
+                        new Tax() { CGST = 0.06, SGST = 0.06 },
+                        new Tax() { CGST = 0.09, SGST = 0.09 }
+                    };
+                }
+                return _taxRateList;
+            }
+            set
+            {
+                SetProperty(ref _taxRateList, value);
+            }
+        }
+        private List<UOM> _uomList;
+        public List<UOM> UOMList
+        {
+            get
+            {
+                if (_uomList == null)
+                {
+                    _uomList = new List<UOM>()
+                    {
+                        new UOM() { Id =1, Name = "Packet" },
+                        new UOM() { Id =2, Name = "Piece" },
+                        new UOM() { Id =3, Name = "Gram" },
+                        new UOM() { Id =4, Name = "Litre" }
+                    };
+                }
+                return _uomList;
+            }
+            set
+            {
+                SetProperty(ref _uomList, value);                
+            }
+        }
 
         //Add other data capture items for product
         public DelegateCommand AddProductCmd { get; set; }
@@ -28,6 +71,8 @@ namespace BakerSoft.ViewModels
         {
             _productModel = productModel;
             AddProductCmd = new DelegateCommand(AddProduct);
+
+            
         }
 
         private void AddProduct()
@@ -35,8 +80,8 @@ namespace BakerSoft.ViewModels
             var product = new Product();
             product.Name = ProductName;
             product.SearchId = Convert.ToInt32(ProductSearchId);
-            //product.Tax = 
-            _productModel.Add();
+             
+            _productModel.Add(product);
         }
     }
 }
