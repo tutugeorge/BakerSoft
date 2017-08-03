e23505f784f85b4bd68f181bf2271b8ee43f8bde
Tutu George <tutugeorge@gmail.com>
2017-08-03 20:59:29 +0530

    Updated AddProduct

--- BakerSoft/ViewModels/AddProductViewModel.cs
+++ BakerSoft/ViewModels/AddProductViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using BakerSoft.Models;
 using GSTBill.Models;
 using GSTBill.ViewModels;
+using log4net;
 using Prism.Commands;
 using System;
 using System.Collections.Generic;
@@ -12,12 +13,46 @@ namespace BakerSoft.ViewModels
 {
     class AddProductViewModel : BaseViewModel
     {
+        private static readonly ILog log = LogManager.GetLogger(
+                System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private ProductModel _productModel;
 
-        public string ProductSearchId { get; set; }
-        public string ProductName { get; set; }
-        public string ProductDescription { get; set; }
-        public string SellingPrice { get; set;}
+        private int _selectedUOM;
+        public int SelectedUOM
+        {
+            get { return _selectedUOM; }
+            set { SetProperty(ref _selectedUOM, value); }
+        }
+        private int _selectedTaxRate;
+        public int SelectedTaxRate
+        {
+            get { return _selectedTaxRate; }
+            set { SetProperty(ref _selectedTaxRate, value); }
+        }
+        private string _productSearchId;
+        public string ProductSearchId
+        {
+            get { return _productSearchId; }
+            set { SetProperty(ref _productSearchId, value); }
+        }
+        private string _productName;
+        public string ProductName
+        {
+            get { return _productName; }
+            set { SetProperty(ref _productName, value); }
+        }
+        private string _productDescription;
+        public string ProductDescription
+        {
+            get { return _productDescription; }
+            set { SetProperty(ref _productDescription, value); }
+        }
+        private string _sellingPrice;
+        public string SellingPrice
+        {
+            get { return _sellingPrice; }
+            set { SetProperty(ref _sellingPrice, value); }
+        }
         public Tax ProductTax { get; set; }
         public List<Price> PriceList { get; set; }
         private List<Tax> _taxRateList;
@@ -29,9 +64,9 @@ namespace BakerSoft.ViewModels
                 {
                     _taxRateList = new List<Tax>()
                     {
-                        new Tax() { CGST = 0.05, SGST = 0.05 },
-                        new Tax() { CGST = 0.06, SGST = 0.06 },
-                        new Tax() { CGST = 0.09, SGST = 0.09 }
+                        new Tax() { CGST = 0.05, SGST = 0.05 , TaxId = 1},
+                        new Tax() { CGST = 0.06, SGST = 0.06 , TaxId = 2},
+                        new Tax() { CGST = 0.09, SGST = 0.09, TaxId = 3 }
                     };
                 }
                 return _taxRateList;
@@ -78,10 +113,40 @@ namespace BakerSoft.ViewModels
         private void AddProduct()
         {
             var product = new Product();
-            product.Name = ProductName;
-            product.SearchId = Convert.ToInt32(ProductSearchId);
-             
-            _productModel.Add(product);
+            try
+            {                
+                product.Name = ProductName;
+                product.SearchId = Convert.ToInt32(ProductSearchId);
+                product.UoM = SelectedUOM;
+                product.PriceList = new List<Price>()
+                { new Price() { SellingPrice = Convert.ToDouble(this.SellingPrice) } };
+                product.Description = ProductDescription;
+                var taxRate = new Tax()
+                {
+                    TaxId = SelectedTaxRate
+                };
+                product.ProductTax = taxRate;
+                _productModel.Add(product);
+                log.Info(String.Format("New product {0} added successfully", ProductDescription ));
+            }
+            catch (Exception ex)
+            {
+                log.Error(product, ex);
+            }
+            finally
+            {
+                ResetUI();
+            }
+        }
+
+        private void ResetUI()
+        {
+            ProductName = "";
+            ProductSearchId = "";
+            SelectedUOM = 0;
+            SellingPrice = "0.00";
+            ProductDescription = "";
+            SelectedTaxRate = 0;
         }
     }
 }
