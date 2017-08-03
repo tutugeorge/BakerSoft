d57c47949a8821653626c33df2eff50997c20229
snehgin <snehgin@gmail.com>
2017-08-03 06:21:43 +0530

    Product DB and Automapper

---
+++ BakerSoft/AutoMapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using DAL.Models;
+using GSTBill.Models;
+
+namespace BakerSoft
+{
+    public static class AutoMapperConfig
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(
+                config =>
+                {
+                    config.CreateMap<Product, PRODUCT>()
+                    .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Id))
+                    .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Name))
+                    .ForMember(dest => dest.ProductDescription, opt => opt.MapFrom(src => src.Description))
+                    .ForMember(dest => dest.ProductCategoryId, opt => opt.MapFrom(src => src.CategoryId))
+                    .ForMember(dest => dest.ProductSearchId, opt => opt.MapFrom(src => src.SearchId))
+                    .ForMember(dest => dest.ProductUoM, opt => opt.MapFrom(src => src.UoM))
+                    .ForMember(dest => dest.ProductType, opt => opt.MapFrom(src => src.Type));
+                });
+        }
+    }
+}
--- BakerSoft/Repositories/ProductRepository.cs
+++ BakerSoft/Repositories/ProductRepository.cs
@@ -5,24 +5,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GSTBill.Models;
-
+using DAL.Models;
+using AutoMapper;
 namespace BakerSoft.Repositories
 {
     class ProductRepository : IProductRepository
     {        
         public void AddProduct(Product product)
         {
-            throw new NotImplementedException();
+            using (var db = new StoreDB())
+            {
+                PRODUCT prod = Mapper.Map<PRODUCT>(product);
+                prod.ProductCategoryId = 1;
+                prod.ProductType = 1;
+                prod.ProductUoM = 1;
+
+                db.PRODUCTS.Add(prod);
+                db.SaveChanges();
+            }
         }
 
         public List<Product> GetProductsById(string id)
         {
-            throw new NotImplementedException();
+            using (var db = new StoreDB())
+            {
+                var query = (from b in db.PRODUCTS
+                             where b.ProductId == Convert.ToInt16(id)
+                             select b);
+                List<Product> prods = Mapper.Map<List<Product>>(query.ToList());
+                return prods;
+            }
         }
 
         public List<Product> GetProductsByName(string name)
         {
-            throw new NotImplementedException();
+            using (var db = new StoreDB())
+            {
+                var query = (from b in db.PRODUCTS
+                             where b.ProductName.Contains(name)
+                             select b);
+                List<Product> prods = Mapper.Map<List<Product>>(query.ToList());
+                return prods;
+            }
         }
     }
 }
