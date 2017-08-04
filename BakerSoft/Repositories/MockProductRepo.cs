798eecc1d7ed732d15c100805c54a0b785687fa8
Tutu George <tutugeorge@gmail.com>
2017-08-04 23:23:10 +0530

    Updated Sale Txn and Add Product

--- BakerSoft/Repositories/MockProductRepo.cs
+++ BakerSoft/Repositories/MockProductRepo.cs
@@ -16,7 +16,7 @@ namespace BakerSoft.Repositories
                 new Product() { Id=1001, Name="P1", Description="P1DEsc" , UoM=1, SearchId=1001,
                     ProductTax = new Tax() { CGST = 0.05, SGST = 0.05},
                     PriceList = new List<Price>() { new Price() { PurchaseId = 1, PurchasePrice = 100.00, SellingPrice = 120.00 } } },
-                new Product() { Id=1002, Name="P2", Description="P2DEsc" , UoM=1, SearchId=1002,
+                new Product() { Id=1002, Name="P2", Description="P2DEsc" , UoM=2, SearchId=1002,
                     ProductTax = new Tax() { CGST = 0.10, SGST = 0.10},
                     PriceList = new List<Price>() { new Price() { PurchaseId = 1, PurchasePrice = 100.00, SellingPrice = 120.00 } } },
             };
